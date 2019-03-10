a2fc455d26a8b59ad240db7f2448a08045a2275b
李印臣 <jackleehzh@126.com>
2019-03-10 20:11:56 +0800

    第四版
    两个栈共用同一个地址空间，相对于第三版主要修改了栈2的初始化函数、弹出函数和弹入函数。

--- stackSort.c
+++ stackSort.c
@@ -47,16 +47,35 @@ int Push(SAStack *s, ElemType e){
     s->stack[s->top] = e;
     return s->top;
 }
-   
+
+int Init2(SAStack *s){
+    s->top = STACK_LEN;
+    s->bottom = STACK_LEN;
+    s->stack = NULL;
+    return s->top;
+}
+
+ElemType Pop2(SAStack *s){
+    ElemType e = GetTop(*s);
+    s->top++;
+    return e;
+}
+
+int Push2(SAStack *s, ElemType e){
+    s->top--;
+    s->stack[s->top] = e;
+    return s->top;
+}
+
 //栈排序
 //主要思想：从一个无序数据栈中弹出一个元素，如果该元素小于（因为输出时是数组顺序输出）另一个栈的栈顶元素，则把该栈的栈顶数据弹出，直到该元素不再小于栈顶元素。然后，把该元素压栈。
-    int sort(char str[], SAStack *s, SAStack *s2){
+int sort(char str[], SAStack *s, SAStack *s2){
     char *tmp = NULL;
     ElemType e = 0;
     tmp = str;
-
+    int i;
     while(1){
-        while(*tmp > 47){
+        while(*tmp != ' ' && *tmp != '\n'){
             e = (e << 3) + (e << 1);
             e += *tmp - 48;
             tmp++;
@@ -65,19 +84,17 @@ int Push(SAStack *s, ElemType e){
         //如果大于栈顶1，则出栈1，入栈2，然后入栈1
         if(!IsEmpty(*s) && e <= GetTop(*s)){
             while(!IsEmpty(*s2) && e < GetTop(*s2)){
-                Push(s, Pop(s2));
+                Push(s, Pop2(s2));
             }
         }else{
             while(!IsEmpty(*s) && e > GetTop(*s)){
-                Push(s2, Pop(s));
+                Push2(s2, Pop(s));
             }
         }
-        
         Push(s, e);
-        
         if(*tmp == '\n'){
             while(!IsEmpty(*s2)){
-                Push(s, Pop(s2));
+                Push(s, Pop2(s2));
             }
             break;
         }
@@ -95,7 +112,6 @@ int main(int argc, char *argv[]){
     SAStack s;
     SAStack s2;
     ElemType elements[STACK_LEN];
-    ElemType elements2[STACK_LEN];
     
     scanf("%d", &n);
     getchar();
@@ -105,13 +121,13 @@ int main(int argc, char *argv[]){
     
     for (i = n - 1 ; i >= 0; i--) {
         elements[i] = 0;
-        elements2[i] = 0;
     }
     
     Init(&s);
-    Init(&s2);
+//注意：调试时出现段错误，因为栈2没有使用修改后的初始化函数。
+    Init2(&s2);
     s.stack = elements;
-    s2.stack = elements2;
+    s2.stack = elements;
     
     sort(str, &s, &s2);
     for (i = n - 1 ; i >= 0; i--) {
